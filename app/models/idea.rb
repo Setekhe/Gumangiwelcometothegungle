2c5ea29201c2159cb066105359b51e402a5e9506
Setekhe <s3th41n@gmail.com>
2018-07-02 10:48:03 +0100

    Semi-final home page

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,4 +3,12 @@ class Idea < ApplicationRecord
                    length: { minimum: 3 }
   validates :details, presence: true,
                    length: { minimum: 10 }
+
+  def formatted_details
+    if details.chars.count > 50
+      details[0..50] + '...'
+    else
+      details
+    end
+  end
 end
