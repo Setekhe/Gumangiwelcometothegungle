3046811d264b641fd6999954db28e8da0eb76170
Setekhe <s3th41n@gmail.com>
2018-07-03 17:44:17 +0100

    searchy boi dono

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @ideas = Idea.includes (params[:search] )
+    @ideas = Idea.where("idea_title like '%#{params[:search]}%' OR details like '%#{params[:search]}%' OR name like '%#{params[:search]}%' ")
   end
 end
