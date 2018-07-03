19aae6246db2d302fb0309b4969e440a8f36efae
Setekhe <s3th41n@gmail.com>
2018-07-03 15:22:35 +0100

    the first admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_143239) do
+ActiveRecord::Schema.define(version: 2018_07_03_141809) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_07_02_143239) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
 end
