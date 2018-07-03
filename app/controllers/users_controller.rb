19668054fc07a95a1c2ba825585f31098ccad7a2
Setekhe <s3th41n@gmail.com>
2018-07-03 09:14:53 +0100

    sign up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "You signed up successfully"
-      flash[:color]= "valid"
+      render 'signed_up'
     else
-      flash[:notice] = "Form is invalid"
-      flash[:color]= "invalid"
+      render 'new'
     end
-    render "new"
+  end
+
+  def user_params
+    params.require(:user).permit(:user_name, :display_name, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users, only: [:new, :create]
+  
   get 'ideas/index'
   resources :ideas do
     resources :comments
